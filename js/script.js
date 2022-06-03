a9baa74f9706598a6ac663a42f959be7e59db0d9
MandoDB <77023822+MandoDB@users.noreply.github.com>
2022-06-03 13:10:44 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 (function () {
     if (!sessionStorage.getItem('cookieconsent')) {
-        var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var data = JSON.parse(request.responseText);
-                var eu_country_codes = ['AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR', 'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PO', 'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA'];
-                if (eu_country_codes.indexOf(data.countryCode) != -1) {
                     document.body.innerHTML += '\
 					<div class="cookieconsent" style="font-family:sans-serif;position:fixed;padding:20px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999;">\
 						Ce site n\'est pas à vendre et je n\'autorise aucune utilisation de ce code à but commercial. Le site est encore en construction.\
@@ -18,9 +12,4 @@
                         sessionStorage.setItem('cookieconsent', true);
                     };
                 }
-            }
-        };
-        request.open('GET', 'http://ip-api.com/json', true);
-        request.send();
-    }
 })();
